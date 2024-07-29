f4bfca983495b1f32e7b30a38d27c319b58322a7
Manish Kumar <mmm.manish163@gmail.com>
2024-07-30 00:06:46 +0530

    fronted parts

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link className="link" to="/">
+          <span className="logo">Booking</span>
+        </Link>
+
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
